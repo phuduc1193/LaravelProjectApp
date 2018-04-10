851ce4630d9d4700aab48bf2d3a8aecbc3a2c831
Duc Truong <phuduc1193@gmail.com>
2018-04-10 23:18:41 +0700

    Add JWT Auth

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function formatValidationErrors(Validator $validator)
+    {
+        $status = 422;
+        return [
+            "message" => $status . " error",
+            "errors" => [
+                "message" => $validator->getMessageBag()->first(),
+                "info" => [$validator->getMessageBag()->keys()[0]],
+            ],
+            "status_code" => $status
+        ];
+    }
 }
--- docker.sh
+++ docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+# php artisan jwt:secret
 cd laradock && docker-compose up -d nginx mysql redis
 docker-compose exec workspace php artisan migrate
